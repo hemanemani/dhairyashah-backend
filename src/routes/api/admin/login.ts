70d814c435b4ca820ea0bdb30512629232222b09
hemanemani <hemanemani@orgenik.com>
2025-07-12 11:37:36 +0530

    adjusted core data2

--- src/routes/api/admin/login.ts
+++ src/routes/api/admin/login.ts
@@ -12,41 +12,43 @@ const pool = new Pool({
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
 
-router.post("/",async(req,res): Promise<void>=>{
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    const {email,password} = req.body;
-        console.log("Login req body:", req.body);
+    console.log("BODY:", req.body);
 
-    try {
+    const result = await pool.query("SELECT * FROM admin WHERE email = $1", [email]);
+    const user = result.rows[0];
 
-        const result = await pool.query(`SELECT * from admins WHERE email = $1`,[email]);
-        const user = result.rows[0];
+    console.log("User from DB:", user);
 
-            console.log("Found user:", user);
-
-
-        if (!user) {
-             res.status(401).json({ message: "Invalid email or password" });
-             return;
-        }
-        const isMatch = await bcrypt.compare(password, user.password_hash)
-
-        if (!isMatch) {
-             res.status(401).json({ message: "Invalid email or password" });
-             return;
-        }
-
-        const token = jwt.sign({user_id:user.id},JWT_SECRET,{expiresIn:"1h"})
-
-        res.json({token})
-
-        return;
-        
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server error" });
+    if (!user) {
+      res.status(400).json({ error: "User not found" });
+      return;
     }
 
-})
+    if (!password || !user.password) {
+      console.log("Missing data or hash:", password, user?.password);
+       res.status(400).json({ error: "Missing password or stored hash" });
+       return
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+       res.status(401).json({ error: "Invalid credentials" });
+       return
+    }
+
+     res.status(200).json({ message: "Login successful" });
+     return
+  } catch (error) {
+    console.error("Login error:", error);
+     res.status(500).json({ error: "Internal server error" });
+     return
+  }
+});
+
 
 export default router;
