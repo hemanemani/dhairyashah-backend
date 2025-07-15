70d02cef6da31e7765656c6534cd307c1e55c9a8
hemanemani <hemanemani@orgenik.com>
2025-07-15 21:01:02 +0530

    changes server file

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,7 @@ app.use("/api/admin/upload",uploadRoutes);
 app.use("/api/admin/message",messageRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("server running on port 5000");
 });
